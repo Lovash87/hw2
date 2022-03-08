1220b71dba446f9049a398a1434a3bdb3f7e440d
Vladimir Ovodov <100378594+Lovash87@users.noreply.github.com>
2022-03-08 15:42:53 +0700

    first commit

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -3,6 +3,16 @@ package ru.skypro;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// задание 3
+
+        var bananasWeight = 5* 80;
+        var icecreamWeight = 105 * 2;
+        var icemilkWeiht = 100 * 2;
+        var eggsWeigt = 4 * 70;
+        var productWeight = bananasWeight + icecreamWeight + icemilkWeiht + eggsWeigt;
+        float grPerKg = 1000;
+        float weihtKg = productWeight / (float)grPerKg;
+
+        System.out.println("Общий вес " + weihtKg + "гр ");
     }
 }
